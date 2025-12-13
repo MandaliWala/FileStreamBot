fa19887022641eb6296f070294912526b81f716e
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-12-13 22:25:12 +0530

    Update start.py

--- FileStream/bot/plugins/start.py
+++ FileStream/bot/plugins/start.py
@@ -16,6 +16,8 @@ db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
 @FileStream.on_message(filters.command('start') & filters.private)
 async def start(bot: Client, message: Message):
+    if not await verify_user(bot, message):
+        return
     usr_cmd = message.text.split("_")[-1]
 
     if usr_cmd == "/start":
